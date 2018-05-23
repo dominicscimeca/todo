0b7ce8a083b91b982eeeb68f06f35b579975a8aa
dscimeca <dscimeca@cj.com>
2018-05-23 15:42:28 -0700

    removing unneeded @throws on App

--- user-service/src/main/scala/com/cj/dscimeca/onboarding/todo/user/App.scala
+++ user-service/src/main/scala/com/cj/dscimeca/onboarding/todo/user/App.scala
@@ -1,15 +1,10 @@
 package com.cj.dscimeca.onboarding.todo.user
 
-import java.io.IOException
-
-import javax.servlet.ServletException
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 
 class App extends HttpServlet {
 
-  @throws(classOf[ServletException])
-  @throws(classOf[IOException])
   override protected def service(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
     DI.RestHandlerManager.handle(req, resp)
   }
